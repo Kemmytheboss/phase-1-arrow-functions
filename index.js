2d4ea74debcf98c90754ac94e13289916ab84017
verah mokaya <verahmokaya475@gmail.com>
2025-09-03 14:13:45 +0300

    arrow functions completed

---
+++ index.js
@@ -0,0 +1,11 @@
+// funciton expression
+const divide = function(){
+    return 2000 /100;
+} 
+
+// arrow function 
+const square = x => x **x;
+square(2,3);
+
+const add = (a,b) => a + b;
+add(1,2);
